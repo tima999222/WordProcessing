27677067173e9bec80a00d347cf6e7ce8cb5bd04
Тимофей <tima.antya@mail.ru>
2023-05-10 15:41:42 +0300

    Добавьте файлы проекта.

---
+++ WordProcessing/CheckGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using PropertiesReflection;
+
+namespace WordProcessing
+{
+    public class CheckGenerator
+    {
+        public void GenereateCheck<T>(string pathTemplate, string pathDest, T obj) where T : class
+        {
+            File.Copy(@"C:\Users\Тимофей\Desktop\check.docx", @"C:\Users\Тимофей\Desktop\check1.docx");
+
+            using (WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(@"C:\Users\Тимофей\Desktop\check1.docx", true))
+            {
+                var props = obj.GetProperties();
+
+                if (wordprocessingDocument != null)
+                {
+                    var texts = wordprocessingDocument.MainDocumentPart.Document.Body.Descendants<Text>();
+
+                    foreach (Text text in texts)
+                    {
+                        foreach (var p in props)
+                        {
+                            if (("{" + p.Name + "}").Contains(text.Text))
+                            {
+                                text.Text = p.GetValue(obj).ToString();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GenereateCheck<T>(string pathTemplate, string pathDest, T obj, List<T> values) where T : class
+        {
+            File.Copy(@"C:\Users\Тимофей\Desktop\check.docx", @"C:\Users\Тимофей\Desktop\check1.docx");
+
+            using (WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(@"C:\Users\Тимофей\Desktop\check1.docx", true))
+            {
+                var props = obj.GetProperties();
+
+                if (wordprocessingDocument != null)
+                {
+                    var texts = wordprocessingDocument.MainDocumentPart.Document.Body.Descendants<Text>();
+                    var table = wordprocessingDocument.MainDocumentPart.Document.Body.Descendants<Table>().FirstOrDefault();
+
+                    if (table != null)
+                    {
+                        Console.WriteLine("Ебать таблица");
+                    }
+
+                    foreach (Text text in texts)
+                    {
+                        foreach (var p in props)
+                        {
+                            if (("{" + p.Name + "}").Contains(text.Text))
+                            {
+                                text.Text = p.GetValue(obj).ToString();
+                            }
+                        }
+                    }
+
+
+                }
+            }
+        }
+    }
+}
