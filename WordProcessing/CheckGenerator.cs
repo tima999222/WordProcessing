a47694288736dfd16a748c91244cfdd22c429ab3
Тимофей <121519813+tima999222@users.noreply.github.com>
2023-05-13 16:15:46 +0300

    Update CheckGenerator.cs

--- WordProcessing/CheckGenerator.cs
+++ WordProcessing/CheckGenerator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using PropertiesReflection;
