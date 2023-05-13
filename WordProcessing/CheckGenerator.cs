46f5219cf6f3801d626bfccb0f88bb13481d57f6
Тимофей <121519813+tima999222@users.noreply.github.com>
2023-05-13 16:15:23 +0300

    Update CheckGenerator.cs

--- WordProcessing/CheckGenerator.cs
+++ WordProcessing/CheckGenerator.cs
@@ -35,39 +35,5 @@ namespace WordProcessing
                 }
             }
         }
-
-        public void GenereateCheck<T>(string pathTemplate, string pathDest, T obj, List<T> values) where T : class
-        {
-            File.Copy(@"C:\Users\Тимофей\Desktop\check.docx", @"C:\Users\Тимофей\Desktop\check1.docx");
-
-            using (WordprocessingDocument wordprocessingDocument = WordprocessingDocument.Open(@"C:\Users\Тимофей\Desktop\check1.docx", true))
-            {
-                var props = obj.GetProperties();
-
-                if (wordprocessingDocument != null)
-                {
-                    var texts = wordprocessingDocument.MainDocumentPart.Document.Body.Descendants<Text>();
-                    var table = wordprocessingDocument.MainDocumentPart.Document.Body.Descendants<Table>().FirstOrDefault();
-
-                    if (table != null)
-                    {
-                        Console.WriteLine("Ебать таблица");
-                    }
-
-                    foreach (Text text in texts)
-                    {
-                        foreach (var p in props)
-                        {
-                            if (("{" + p.Name + "}").Contains(text.Text))
-                            {
-                                text.Text = p.GetValue(obj).ToString();
-                            }
-                        }
-                    }
-
-
-                }
-            }
-        }
     }
 }
